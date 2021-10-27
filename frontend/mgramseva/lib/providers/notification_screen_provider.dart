62fc292ad0a29d712c0bb2e5001d0ebaf2b32f4e
RamkrishnaEgovt <ramkrishna.sahoo@egovernments.org>
2021-10-27 10:58:08 +0530

    IFIX-658

--- frontend/mgramseva/lib/providers/notification_screen_provider.dart
+++ frontend/mgramseva/lib/providers/notification_screen_provider.dart
@@ -87,8 +87,9 @@ class NotificationScreenProvider with ChangeNotifier {
           ((offset + limit) - 1) > totalCount
               ? totalCount
               : (offset + limit) - 1));
-    } catch (e) {
-      print(e);
+    } catch (e, s) {
+      ErrorHandler().allExceptionsHandler(navigatorKey.currentContext!, e, s);
+      streamController.addError('error');
     }
   }
 
@@ -97,7 +98,8 @@ class NotificationScreenProvider with ChangeNotifier {
     try {
       getNotifications(response.offset, response.limit);
     } catch (e, s) {
-      ErrorHandler().allExceptionsHandler(navigatorKey.currentContext!, e);
+      ErrorHandler().allExceptionsHandler(navigatorKey.currentContext!, e, s);
+      streamController.addError('error');
     }
   }
 
