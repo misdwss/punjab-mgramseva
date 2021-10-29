c355456e938ae8b49f9878b8f05370a050120337
naveen-wt-egov <90044067+naveen-wt-egov@users.noreply.github.com>
2021-10-29 20:39:39 +0530

    added null checks

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/builder/WsQueryBuilder.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/builder/WsQueryBuilder.java
@@ -241,20 +241,20 @@ public class WsQueryBuilder {
 			query.append(" conn.locality = ? ");
 			preparedStatement.add(criteria.getLocality());
 		}
-		if ((criteria.getIsCollectionCount() && criteria.getIsBillPaid() != null)
-				&& criteria.getIsCollectionDataCount() == false) {
-			StringBuilder paidOrPendingQuery = new StringBuilder("with td as (");
-			paidOrPendingQuery.append(query).append("{orderby}").append(") ")
-					.append("select count(*) OVER() AS full_count, * from td where ");
-
-			if (criteria.getIsBillPaid()) {
-				paidOrPendingQuery.append(" pendingamount <= ? ").append(" or pendingamount is null");
-				preparedStatement.add(0);
-			} else {
-				paidOrPendingQuery.append(" pendingamount > ? ");
-				preparedStatement.add(0);
+		if(criteria.getIsCollectionCount() != null && criteria.getIsCollectionDataCount() != null) {
+			if((criteria.getIsCollectionCount() && criteria.getIsBillPaid() != null) && criteria.getIsCollectionDataCount() == false) {
+				StringBuilder paidOrPendingQuery = new StringBuilder("with td as (");
+				paidOrPendingQuery.append(query).append("{orderby}").append(") ").append("select count(*) OVER() AS full_count, * from td where ");
+			
+				if(criteria.getIsBillPaid()) {
+					paidOrPendingQuery.append(" pendingamount <= ? ").append(" or pendingamount is null");
+					preparedStatement.add(0);
+				}else {
+					paidOrPendingQuery.append(" pendingamount > ? ");
+					preparedStatement.add(0);
+				}
+				query = paidOrPendingQuery.append("{pagination}");
 			}
-			query = paidOrPendingQuery.append("{pagination}");
 		}
 
 		return query;
