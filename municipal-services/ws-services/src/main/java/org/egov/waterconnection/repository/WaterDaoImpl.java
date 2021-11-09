4662eabc0aff68d6d176009b5edf07f5a8122415
naveen-wt-egov <naveen.gupta@egovernments.org>
2021-11-09 13:18:39 +0530

    change filter.

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/WaterDaoImpl.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/repository/WaterDaoImpl.java
@@ -127,7 +127,7 @@ public class WaterDaoImpl implements WaterDao {
 			if (criteria.getIsPropertyCount()!= null && criteria.getIsPropertyCount()) {
 				List<Object> preparedStmnt = new ArrayList<>();
 				StringBuilder propertyQuery = new StringBuilder(wsQueryBuilder.PROPERTY_COUNT);
-				propertyQuery = wsQueryBuilder.applyFiltersForFuzzySearch(propertyQuery, preparedStmnt, criteria);
+				propertyQuery = wsQueryBuilder.applyFilters(propertyQuery, preparedStmnt, criteria);
 				propertyQuery.append("GROUP BY additionaldetails->>'propertyType'");
 				List<Map<String, Object>> data = jdbcTemplate.queryForList(propertyQuery.toString(),
 						preparedStmnt.toArray());
