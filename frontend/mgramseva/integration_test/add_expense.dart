b638535740076a2de114f6dfbdfb137587ddc57d
krishnaji-eGov <87766660+krishnaji-eGov@users.noreply.github.com>
2021-11-12 08:35:54 +0530

    IFIX-694

--- frontend/mgramseva/integration_test/add_expense.dart
+++ frontend/mgramseva/integration_test/add_expense.dart
@@ -5,7 +5,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 // TODO 5: Import the app that you want to test
 import 'package:mgramseva/main.dart' as app;
+import 'package:mgramseva/utils/Locilization/application_localizations.dart';
 import 'package:mgramseva/utils/TestingKeys/testing_keys.dart';
+import 'package:mgramseva/utils/global_variables.dart';
 
 
 void main() {
@@ -19,13 +21,19 @@ void main() {
 
       final phoneNumber = find.byKey(Keys.expense.VENDOR_MOBILE_NUMBER);
       final vendorName = find.byKey(Keys.expense.VENDOR_NAME);
-      final expenseType = find.byKey(Keys.expense.EXPENSE_TYPE);
       final expenseAmount = find.byKey(Keys.expense.EXPENSE_AMOUNT);
       final expenseBillDate = find.byKey(Keys.expense.EXPENSE_BILL_DATE);
       final addExpenseBtn = find.byKey(Keys.expense.EXPENSE_SUBMIT);
       final backBtn = find.byKey(Keys.expense.BACK_BTN);
+      final expenseType = find.byKey(Keys.expense.EXPENSE_TYPE);
 
-      // await tester.enterText(phoneNumber, '9686151676');
+      final selectExpenseType = find.widgetWithText(ListTile, ApplicationLocalizations.of(navigatorKey.currentContext!).translate('SALARY'));
+      await tester.ensureVisible(expenseType);
+      await tester.tap(expenseType);
+      await tester.pumpAndSettle(Duration(milliseconds: 3000));
+      await tester.tap(selectExpenseType);
+      await tester.pumpAndSettle(Duration(milliseconds: 3000));
+
       await tester.enterText(vendorName, 'Singer');
       await tester.enterText(expenseType, 'SALARY');
       await tester.enterText(expenseAmount, '123');
--- frontend/mgramseva/integration_test/update_expense.dart
+++ frontend/mgramseva/integration_test/update_expense.dart
@@ -5,7 +5,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 // TODO 5: Import the app that you want to test
 import 'package:mgramseva/main.dart' as app;
+import 'package:mgramseva/utils/Locilization/application_localizations.dart';
 import 'package:mgramseva/utils/TestingKeys/testing_keys.dart';
+import 'package:mgramseva/utils/global_variables.dart';
+import 'package:mgramseva/widgets/BottonButtonBar.dart';
+import 'package:mgramseva/widgets/ShortButton.dart';
+import 'package:mgramseva/utils/Constants/I18KeyConstants.dart';
 
 
 void main() {
@@ -19,9 +24,11 @@ void main() {
 
     /// Searching by vendor name
     final vendorName = find.byKey(Keys.expense.SEARCH_VENDOR_NAME);
-    final updateExpenditure = find.byKey(Keys.expense.UPDATE_EXPNEDITURE);
+    final updateExpenditure = find.widgetWithText(ShortButton, ApplicationLocalizations.of(navigatorKey.currentContext!).translate(i18.expense.UPDATE_EXPENDITURE)).first;
+    final checkBox = find.byType(Checkbox);
+    final submitButton = find.widgetWithText(BottomButtonBar, ApplicationLocalizations.of(navigatorKey.currentContext!).translate(i18.common.SUBMIT));
 
-    await tester.enterText(vendorName, 'Singer');
+    await tester.enterText(vendorName, 'Raja');
     await tester.pumpAndSettle(Duration(milliseconds: 2000));
 
     await tester.tap(find.byType(ElevatedButton));
@@ -29,8 +36,22 @@ void main() {
 
     /// picking the first expense from the result
     await tester.ensureVisible(updateExpenditure);
+    await tester.pumpAndSettle(Duration(milliseconds: 2000));
     await tester.tap(updateExpenditure);
     await tester.pumpAndSettle(Duration(seconds: 5));
 
+    await tester.ensureVisible(checkBox);
+    await tester.pumpAndSettle(Duration(milliseconds: 2000));
+    await tester.tap(checkBox);
+    await tester.pumpAndSettle(Duration(seconds: 3));
+
+    await tester.ensureVisible(submitButton);
+    await tester.pumpAndSettle(Duration(milliseconds: 2000));
+    await tester.tap(submitButton);
+    await tester.pumpAndSettle(Duration(seconds: 8));
+
+    final backHome = find.widgetWithText(BottomButtonBar, ApplicationLocalizations.of(navigatorKey.currentContext!).translate(i18.common.BACK_HOME));
+    await tester.tap(backHome);
+    await tester.pumpAndSettle(Duration(seconds: 5));
   });
 }
