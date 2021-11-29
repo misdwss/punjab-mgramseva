9a20e3bbefbf33ebf16226df126af172573d3e50
naveen-wt-egov <90044067+naveen-wt-egov@users.noreply.github.com>
2021-11-29 21:41:36 +0530

    Ng punjab mgramseva develop v5 (#79)
    * added logs for test adaptor request.
    
    * added checks for paid amount is zero for create payment topic adaptor.
    
    * adapter changes fixes for bill payment.
    
    * removed else condition
    
    * adapter changes for duplicate bill generation.
    
    * added db config for adpator.
    
    * adpator fixes.
    
    * removed logs statement.
    
    * added cancel payment topic for listen.

--- municipal-services/mgramseva-ifix-adapter/src/main/java/org/egov/mgramsevaifixadaptor/consumer/MgramsevaAdapterDemandConsumer.java
+++ municipal-services/mgramseva-ifix-adapter/src/main/java/org/egov/mgramsevaifixadaptor/consumer/MgramsevaAdapterDemandConsumer.java
@@ -1,9 +1,11 @@
 package org.egov.mgramsevaifixadaptor.consumer;
 
 import java.math.BigDecimal;
+import java.text.Bidi;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 import org.egov.mgramsevaifixadaptor.config.PropertyConfiguration;
 import org.egov.mgramsevaifixadaptor.contract.DemandRequest;
@@ -15,6 +17,8 @@ import org.egov.mgramsevaifixadaptor.models.EventTypeEnum;
 import org.egov.mgramsevaifixadaptor.util.Constants;
 import org.egov.mgramsevaifixadaptor.util.MgramasevaAdapterWrapperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -32,10 +36,14 @@ public class MgramsevaAdapterDemandConsumer {
 	@Autowired
 	MgramasevaAdapterWrapperUtil util;
 	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	@KafkaListener(topics = { "${kafka.topics.create.demand}"})
 	public void listen(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		DemandRequest demandRequest=null;
+		log.info("crate demand topic");
 		try {
 			log.debug("Consuming record: " + record);
 			demandRequest = mapper.convertValue(record, DemandRequest.class);
@@ -59,16 +67,12 @@ public class MgramsevaAdapterDemandConsumer {
 	public void listenUpdate(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		DemandRequest demandRequest=null;
+		log.info("update demand topic");
 		try {
 			log.debug("Consuming record: " + record);
 			demandRequest = mapper.convertValue(record, DemandRequest.class);
+			log.info("demandRequest: "+demandRequest);
 			String eventType=null;
-			if(demandRequest.getDemands().get(0).getBusinessService().contains(Constants.EXPENSE))
-			{
-				eventType=EventTypeEnum.BILL.toString();
-			}else {
-				eventType=EventTypeEnum.DEMAND.toString();
-			}
 			if(demandRequest != null) {
 				Collections.sort(demandRequest.getDemands(), getCreatedTimeComparatorForDemand());
 				if(demandRequest.getDemands().get(0).getStatus().toString().equalsIgnoreCase(Constants.CANCELLED)) {
@@ -89,7 +93,17 @@ public class MgramsevaAdapterDemandConsumer {
 					for(int i=0; i<demandDetailsSize-1; i++) {
 						demandRequest.getDemands().get(0).getDemandDetails().remove(0);
 					}
+					Integer count =  getCountByDemandDetailsId(demandRequest.getDemands().get(0).getDemandDetails().get(0).getId());
+					if(count != null && count > 1) {
+						return;
+					}
 				}
+			}
+			if(demandRequest.getDemands().get(0).getBusinessService().contains(Constants.EXPENSE))
+			{
+				eventType=EventTypeEnum.BILL.toString();
+			}else {
+				eventType=EventTypeEnum.DEMAND.toString();
 			}
 			util.callIFIXAdapter(demandRequest, eventType, demandRequest.getDemands().get(0).getTenantId(),demandRequest.getRequestInfo());
 		} catch (final Exception e) {
@@ -105,4 +119,12 @@ public class MgramsevaAdapterDemandConsumer {
 			}
 		};
 	}
+	
+	public Integer getCountByDemandDetailsId(String demanddetailid) {  
+		String sql = "SELECT count(*) FROM egbs_demanddetail_v1_audit WHERE demanddetailid = '"+demanddetailid+"' ";
+		Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+		System.out.println("count: "+count);
+		return count;
+	}
+	
 }
