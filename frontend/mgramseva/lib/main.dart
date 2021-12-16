cda0dfd35f568ef3e74afbeb7b91daf7e5f17226
RamkrishnaEgovt <ramkrishna.sahoo@egovernments.org>
2021-12-16 12:28:17 +0530

    IFIX-771

--- frontend/mgramseva/lib/main.dart
+++ frontend/mgramseva/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'dart:isolate';
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 
@@ -33,8 +34,10 @@ import 'package:mgramseva/routers/Routers.dart';
 import 'package:mgramseva/screeens/Home/Home.dart';
 
 import 'package:mgramseva/screeens/SelectLanguage/languageSelection.dart';
+import 'package:mgramseva/services/LocalStorage.dart';
 import 'package:mgramseva/theme.dart';
 import 'package:mgramseva/utils/Locilization/application_localizations.dart';
+import 'package:mgramseva/utils/constants.dart';
 import 'package:mgramseva/utils/error_logging.dart';
 import 'package:mgramseva/utils/global_variables.dart';
 import 'package:mgramseva/utils/loaders.dart';
@@ -43,6 +46,7 @@ import 'package:new_version/new_version.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 import 'providers/collect_payment.dart';
@@ -183,28 +187,62 @@ class _LandingPageState extends State<LandingPage> {
     //newVersion.showAlertIfNecessary(context: context); //Use this if you want the update alert with default settings
     final status = await newVersion.getVersionStatus();
 
+    var updateStatus = storage.read(key: Constants.UPDATE_STATUS_KEY);
+    if(updateStatus.toString() == 'updateInitiated' && !status!.canUpdate)
+      {
+        storage.deleteAll();
+      }
+
     if (status != null && status.canUpdate) {
+      final uri =
+      Uri.https("play.google.com", "/store/apps/details", {"id": "com.dwss.mgramseva"});
       int.parse(status.storeVersion.split('.').first) >
               int.parse(status.localVersion.split('.').first)
-          ? newVersion.showUpdateDialog(
-              context: context,
-              versionStatus: status,
-              dialogTitle: 'UPDATE AVAILABLE',
-              updateButtonText: 'Update Now',
-              allowDismissal: false,
-              dismissButtonText: '',
-              dialogText:
-                  'Please update the app from ${status.localVersion} to ${status.storeVersion}',
-            )
-          : newVersion.showUpdateDialog(
-              context: context,
-              versionStatus: status,
-              dialogTitle: 'UPDATE AVAILABLE',
-              updateButtonText: 'Update Now',
-              dismissButtonText: 'Skip',
-              dialogText:
-                  'Please update the app from ${status.localVersion} to ${status.storeVersion}',
+          ? showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return WillPopScope(
+              child: AlertDialog(
+                title: Text('UPDATE AVAILABLE'),
+                content: Text('Please update the app from ${status.localVersion} to ${status.storeVersion}'),
+                actions: [
+                  TextButton(onPressed: () => launchPlayStore(uri.toString()),
+                      child: Text('Update'))
+                ],
+              ),
+              onWillPop: () => Future.value(false)
             );
+          }
+          )
+          : showDialog(
+          context: context,
+          barrierDismissible: true,
+          builder: (BuildContext context) {
+            return WillPopScope(
+                child: AlertDialog(
+                  title: Text('UPDATE AVAILABLE'),
+                  content: Text('Please update the app from ${status.localVersion} to ${status.storeVersion}'),
+                  actions: [
+                    TextButton(onPressed: () => launchPlayStore(uri.toString()),
+                        child: Text('Update')),
+                    TextButton(onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
+                        child: Text('Later'))
+                  ],
+                ),
+                onWillPop: () => Future.value(true)
+            );
+          }
+      );
+    }
+  }
+
+  void launchPlayStore(String appLink) async {
+    storage.write(key: Constants.UPDATE_STATUS_KEY, value: 'updateInitiated');
+    if (await canLaunch(appLink)) {
+      await launch(appLink);
+    } else {
+      throw 'Could not launch appStoreLink';
     }
   }
 
@@ -212,6 +250,10 @@ class _LandingPageState extends State<LandingPage> {
   void dispose() {
     IsolateNameServer.removePortNameMapping('downloader_send_port');
     super.dispose();
+  }
+
+  void showCustomDialog(){
+
   }
 
   static void downloadCallback(
