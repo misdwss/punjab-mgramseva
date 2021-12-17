5a018dc76c7fc3087c355b4ec340f822c34fddc1
RamkrishnaEgovt <ramkrishna.sahoo@egovernments.org>
2021-12-17 18:50:17 +0530

    IFIX-786

--- frontend/mgramseva/lib/providers/dashboard_provider.dart
+++ frontend/mgramseva/lib/providers/dashboard_provider.dart
@@ -567,7 +567,7 @@ class DashBoardProvider with ChangeNotifier {
   }
 
   void onChangeOfPageLimit(PaginationResponse response, BuildContext context) {
-    fetchDetails(context, response.limit, response.offset);
+    fetchDetails(context, response.limit, response.offset, response.isPageChange);
   }
 
   fetchDetails(BuildContext context,
