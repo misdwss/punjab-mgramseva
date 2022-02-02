7c3e984ab0bfd5201f37035b2c8b95dbef455387
lakshmisravani-wtt-egov <80089327+lakshmisravani-wtt-egov@users.noreply.github.com>
2022-02-02 10:57:20 +0530

    Update migrate.sh

--- core-services/egov-pg-service/src/main/resources/db/migrate.sh
+++ core-services/egov-pg-service/src/main/resources/db/migrate.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-<<<<<<< HEAD:municipal-services/property-services/src/main/resources/db/migrate.sh
-flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true -ignoreMissingMigrations=true migrate
-=======
 flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true migrate
->>>>>>> master:core-services/egov-pg-service/src/main/resources/db/migrate.sh
