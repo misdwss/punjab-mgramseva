e4c9fc8fab5d7c78ce06e53584283b6aa8e8a720
krishnaji-eGov <87766660+krishnaji-eGov@users.noreply.github.com>
2022-03-03 11:25:16 +0530

    IFIX-923

--- frontend/mgramseva/lib/main.dart
+++ frontend/mgramseva/lib/main.dart
@@ -73,13 +73,14 @@ void main() {
     };
 
     WidgetsFlutterBinding.ensureInitialized();
-    await FlutterDownloader.initialize(
-        debug: true // optional: set false to disable printing logs to console
-    );
 
     if(Firebase.apps.length == 0) {
       await Firebase.initializeApp();
     }
+
+    await FlutterDownloader.initialize(
+        debug: true // optional: set false to disable printing logs to console
+    );
 
     runApp(MyApp());
   }, (Object error, StackTrace stack) {
