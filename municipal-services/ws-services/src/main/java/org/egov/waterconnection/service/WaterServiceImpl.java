1713d64f905be1ae968eec37356575831b0a7c99
WalkingTree <lakshmi.sravani@egovernments.org>
2022-04-13 19:33:19 +0530

    IFIX-983: Code changes related to main monthly dashboard table data.

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/service/WaterServiceImpl.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/service/WaterServiceImpl.java
@@ -614,24 +614,32 @@ public class WaterServiceImpl implements WaterService {
 		LocalDate currentMonthDate = LocalDate.now();
 
 		Calendar currentDate = Calendar.getInstance();
+		int currentYear = currentDate.get(Calendar.YEAR);
 		currentDate.setTimeInMillis(criteria.getFromDate());
+		int actualYear = currentDate.get(Calendar.YEAR);
+
 		int currentMonthNumber = currentDate.get(Calendar.MONTH);
 
 		int totalMonthsTillDate;
 		LocalDate finYearStarting;
-		if (currentMonthNumber < 3) {
-			totalMonthsTillDate = 9 + currentMonthNumber;
-
+		
+		if (currentYear != actualYear && actualYear < currentYear) {
+			totalMonthsTillDate = 11;
 			currentMonthDate = LocalDate.of(currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH) + 1,
 					currentDate.get(Calendar.DAY_OF_MONTH));
-
 			finYearStarting = currentMonthDate;
 		} else {
-			totalMonthsTillDate = currentMonthNumber - 2;
-			currentMonthDate = LocalDate.of(currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH) + 1,
-					currentDate.get(Calendar.DAY_OF_MONTH));
-
-			finYearStarting = currentMonthDate;
+			if (currentMonthNumber < 3) {
+				totalMonthsTillDate = 9 + currentMonthNumber;
+				currentMonthDate = LocalDate.of(currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH) + 1,
+						currentDate.get(Calendar.DAY_OF_MONTH));
+				finYearStarting = currentMonthDate;
+			} else {
+				totalMonthsTillDate = currentMonthNumber - 2;
+				currentMonthDate = LocalDate.of(currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH) + 1,
+						currentDate.get(Calendar.DAY_OF_MONTH));
+				finYearStarting = currentMonthDate;
+			}
 		}
 		ArrayList<RevenueCollectionData> data = new ArrayList<RevenueCollectionData>();
 
