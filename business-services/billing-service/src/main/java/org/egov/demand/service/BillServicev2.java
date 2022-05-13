86ca337dec965a333bf4d7ee9ef8017668473edc
snehal-eGov <94351369+snehal-eGov@users.noreply.github.com>
2022-05-13 13:15:51 +0530

    Update BillServicev2.java

--- business-services/billing-service/src/main/java/org/egov/demand/service/BillServicev2.java
+++ business-services/billing-service/src/main/java/org/egov/demand/service/BillServicev2.java
@@ -192,7 +192,7 @@ public class BillServicev2 {
 		 * If no existing bills found then Generate new bill 
 		 */
 		if (CollectionUtils.isEmpty(bills))
-			generateBill(billCriteria, requestInfo);
+			return generateBill(billCriteria, requestInfo);
 		
 		/*
 		 * Adding consumer-codes of unbilled demands to generate criteria
