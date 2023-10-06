aa3b01af250318c72aca93091647191e75c9a11e
debasishchakraborty-egovt <debasish@egovernmets.org>
2023-10-06 18:47:22 +0530

    PFM-4871 - Added changes to persist sms before sending to user

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/repository/builder/SmsNotificationRepository.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/repository/builder/SmsNotificationRepository.java
@@ -1,6 +1,7 @@
 package org.egov.web.notification.sms.repository.builder;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Slf4j
 @Repository
 public class SmsNotificationRepository {
-
+    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public static final String SELECT_NEXT_SEQUENCE_USER = "select nextval('seq_eg_notification_sms')";
