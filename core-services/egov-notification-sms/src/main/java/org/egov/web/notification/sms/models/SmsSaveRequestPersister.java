4697499bf326a6c9d0c28bb670d73dd3cd139696
debasishchakraborty-egovt <debasish@egovernmets.org>
2023-10-08 14:07:52 +0530

    Sms request wrapper

---
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/models/SmsSaveRequestPersister.java
@@ -0,0 +1,10 @@
+package org.egov.web.notification.sms.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SmsSaveRequestPersister {
+
+        @JsonProperty("smsSaveRequest")
+        private SmsSaveRequest smsSaveRequest = null;
+
+}
